c9c8b4f02cec8edd561f82019262576ae8a512e6
MXN-MOVIE <mxnmovie@gmail.com>
2025-03-31 18:38:42 +0530

    🎬 MXN-MOVIE 🎬

---
+++ settings.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {    
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=gKNjVToZ#VoeaGBsw0EVP39QeZNhA5F1TxZuV08DC3Gz-R4HhZ_U',
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
+PREFIX:  process.env.PREFIX || ['.'] ,
+DIRECTION: true     
+};
+
