694b1e6563877129209a7aa50de763ced4ca40af
MXN-MOVIE <mxnmovie@gmail.com>
2025-04-01 12:42:53 +0530

    🎬 MXN-MOVIE 🎬

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=gKNjVToZ#VoeaGBsw0EVP39QeZNhA5F1TxZuV08DC3Gz-R4HhZ_U',
+SESSION_ID: process.env.SESSION_ID || 'Put your session id here',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 DIRECTION: true     
