d8688cf8b6a4c76605baabaa14e319c086abf4fc
MXN-MOVIE <mxnmovie@gmail.com>
2025-04-01 13:33:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'MXN-MOVIE=MScGQBqZ#qvWXsZBkRdHAjH39xUleHyCTZD73CT9HH3KJCVzxTCA',
+SESSION_ID: process.env.SESSION_ID || 'MXN-MOVIE=NLVyVR6Q#g1rBVZ5jrSBU2bj2tp3u10ZFmgSnRIOLAOp_LwIGqOU',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 DIRECTION: true     
